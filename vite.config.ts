102daff1eafac747e01e1b83bded5f72bd4443be
pan <13329870472@163.com>
2025-07-18 17:35:13 +0800

    升级依赖

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ import markdown from 'unplugin-vue-markdown/vite';
 import svgLoader from 'vite-svg-loader';
 import { configDefaults } from 'vitest/config';
 
-const baseUrl = process.env.BASE_URL ?? '/';
+const baseUrl = process.env.BASE_URL ?? './';
 
 // https://vitejs.dev/config/
 export default defineConfig({
